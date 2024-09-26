f4a78197521b1b358d6b395f9eca16c1ed2dfdc1
Armaan044 <102404946+Armaan044@users.noreply.github.com>
2024-09-26 15:59:13 -0400

    Bubble sort

--- main.go
+++ main.go
@@ -11,4 +11,15 @@ func main() {
 func substract(a int, b int) int {
 	return a - b
 }
-
+// Armaandeep Singh
+// Bubble sort
+func bubbleSort(arr []int) 
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        for j := 0; j < n-i-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+    }
+}  
