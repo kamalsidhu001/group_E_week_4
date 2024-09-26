a987fe3ff020ab2378f51daecfce4d3bfde47ea7
kamalsidhu001 <sidhukamla62398@gmail.com>
2024-09-26 15:34:33 -0400

    Push your code

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to GroupE's Week 4 Project")
+	fmt.Println(substract(4, 3))
+}
+
+func substract(a int, b int) int {
+	return a - b
+}
